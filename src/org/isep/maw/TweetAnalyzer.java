460a02a952ce4db668492f0b102f6a094b66b963
slefebvre <slvn.lefebvre@gmail.com>
2014-11-21 17:05:39 +0100

    command line args usage message

--- src/org/isep/maw/TweetAnalyzer.java
+++ src/org/isep/maw/TweetAnalyzer.java
@@ -3,6 +3,7 @@ package org.isep.maw;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import backtype.storm.Config;
@@ -13,44 +14,43 @@ import backtype.storm.tuple.Fields;
 import com.google.common.collect.Lists;
 
 public class TweetAnalyzer {
-	static private int TOP = 10;
-	static private int EMIT_FREQ = 5;
-	static private int WINDOW_SIZE = 15;
-	
+	static private int TOP = 20;
+	static private int EMIT_FREQ = 30;
+	static private int WINDOW_SIZE = 60;
 	
 	private static LocalCluster cluster; 
 	public static void main(String [] args) throws FileNotFoundException {
+		if(args.length <= 2) {
+			System.out.println("Usage: $ tweetAnalyzer top_n path_result path_to_data1 path_to_data2 ..." );
+			System.exit(0);
+		}
+		TOP = Integer.parseInt(args[0]);
+		String resultFilePath = args[1];
+		
 		cluster = new LocalCluster();
-		List<String> fileList = Lists.newArrayList(args);		
-		
-		List<String> pathList = new ArrayList<String>();
-		
+		List<String> fileList = Arrays.asList(args).subList(2, args.length);
+		List<String> pathList = new ArrayList<String>();	
 		
 		for(String relPath: fileList) {
 			File f = new File(relPath);
 			pathList.add(f.getAbsolutePath());
-
 		}
 		
-		
 		TopologyBuilder builder = new TopologyBuilder();
-		
 		builder.setSpout("tweet", new CSVTweetSpout(pathList));
 		builder.setBolt("wordcount", new WordCountBolt(),3)
 					.shuffleGrouping("tweet");
 		builder.setBolt("rank", new RollingWordRank(TOP,EMIT_FREQ,WINDOW_SIZE),3) 
 					.fieldsGrouping("wordcount", new Fields("word"));
-		builder.setBolt("totalranking", new MergeWordsRanks(TOP, EMIT_FREQ, new File("test-results.json").getAbsolutePath()),1)
-					.globalGrouping("rank");
-		
-		
+		builder.setBolt("totalranking", new MergeWordsRanks(TOP, EMIT_FREQ, new File(resultFilePath).getAbsolutePath()),1)
+					.globalGrouping("rank");		
 		//1. build topology
 		Config conf = new Config();
 		conf.setDebug(true);
 		//2. run it for a while
-		cluster.submitTopology("test", conf, builder.createTopology());
+		cluster.submitTopology("MAW 2014", conf, builder.createTopology());
 		try {
-			Thread.sleep(27500);
+			Thread.sleep(47500);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
